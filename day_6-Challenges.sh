b494b0a15ec7e4d7f78e3c413c07ec01028cd7a9
QuaziAyeshaIram <kaziayesha009@gmail.com>
2025-06-18 12:29:11 +0530

    Update day_6-Challenges.sh

--- day_6-Challenges.sh
+++ day_6-Challenges.sh
@@ -6,5 +6,6 @@ current date and time.
 comment
 
 Answer:
-
 echo "hi Ayesha, hello in devops,the current date and time is: $(date)"
+Output:
+hi Ayesha, hello in devops,the current date and time is: Wed Jun 18 12:28:29 PM IST 2025
