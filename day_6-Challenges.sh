cccf6269e187198f4da56c9a3462778645a49712
Ayesha Iram <ayeshactl@gmail.com>
2025-06-18 12:22:24 +0530

    update

---
+++ day_6-Challenges.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+<<comment
+🔹 Challenge 1: Write a simple Bash script that prints “Hello DevOps” along with the 
+current date and time.
+comment
+
+Answer:
+
+echo "hi Ayesha, hello in devops,the current date and time is: $(date)"
